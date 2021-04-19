d7e2cdbb06557e7b1be7753db757b55ef971aef4
Sinzie07 <sinzie@hotmail.com>
2021-04-19 21:19:59 +0700

    layout tambah menu

---
+++ app/src/main/java/suryapro/ustmobile/pos_admin/tambahmenu.java
@@ -0,0 +1,13 @@
+package suryapro.ustmobile.pos_admin;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class tambahmenu extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
