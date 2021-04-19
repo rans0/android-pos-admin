596e137b035e4e18692d507763262e4a52c0814d
dekran <41975320+rans0@users.noreply.github.com>
2021-04-19 23:02:51 +0700

    Fitur baru + Refactor (#2)
    * layout tambah menu
    
    * refactor login-register
    
    * menambahkan fitur tambah menu
    
    Co-authored-by: Sinzie07 <sinzie@hotmail.com>

---
+++ app/src/main/java/suryapro/ustmobile/pos_admin/TambahMenuActivity.java
@@ -0,0 +1,78 @@
+package suryapro.ustmobile.pos_admin;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+public class TambahMenuActivity extends AppCompatActivity {
+
+    private EditText namaMakanan, deskripsiMakanan, hargaMakanan;
+    private Button tambahMenu;
+    private FirebaseFirestore db;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tambah_menu);
+
+        namaMakanan = findViewById(R.id.nama_makanan);
+        deskripsiMakanan = findViewById(R.id.deskripsi);
+        hargaMakanan = findViewById(R.id.harga);
+        tambahMenu = findViewById(R.id.tambah_menu);
+
+        db = FirebaseFirestore.getInstance();
+
+        tambahMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String id = UUID.randomUUID().toString();
+                String nama = namaMakanan.getText().toString();
+                String deskripsi = deskripsiMakanan.getText().toString();
+                String harga = hargaMakanan.getText().toString();
+
+                saveToFireStore(id, nama, deskripsi, harga);
+            }
+        });
+    }
+
+    private void saveToFireStore(String id, String nama, String deskripsi, String harga) {
+        if (!nama.isEmpty() && !deskripsi.isEmpty() && !harga.isEmpty()) {
+            HashMap<String, Object> map = new HashMap<>();
+            map.put("id", id);
+            map.put("nama", nama);
+            map.put("deskripsi", deskripsi);
+            map.put("harga", harga);
+
+            db.collection("DaftarMenu").document(id).set(map)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(TambahMenuActivity.this, "Menu Berhasil Ditambahkan", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(TambahMenuActivity.this, "Gagal Menambahkan Menu", Toast.LENGTH_SHORT).show();
+                }
+            });
+        } else {
+            Toast.makeText(this, "Tidak Boleh Kosong", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
