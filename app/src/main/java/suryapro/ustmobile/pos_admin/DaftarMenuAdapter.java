5430961920b1a964346474223dcf3d5032016898
uzivurt0 <gue.fafa00@gmail.com>
2021-06-08 23:56:15 +0700

    Menambah fungsi delete

--- app/src/main/java/suryapro/ustmobile/pos_admin/DaftarMenuAdapter.java
+++ app/src/main/java/suryapro/ustmobile/pos_admin/DaftarMenuAdapter.java
@@ -1,12 +1,24 @@
 package suryapro.ustmobile.pos_admin;
 
+import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.List;
 
@@ -14,10 +26,14 @@ public class DaftarMenuAdapter extends RecyclerView.Adapter<DaftarMenuAdapter.Me
 
     private DaftarMenuActivity activity;
     private List<ModelMenu> mList;
+    FirebaseFirestore firestore;
+    FirebaseAuth auth;
 
     public DaftarMenuAdapter(DaftarMenuActivity activity, List<ModelMenu> mList) {
         this.activity = activity;
         this.mList = mList;
+        firestore = FirebaseFirestore.getInstance();
+        auth = FirebaseAuth.getInstance();
     }
 
     @NonNull
@@ -32,7 +48,27 @@ public class DaftarMenuAdapter extends RecyclerView.Adapter<DaftarMenuAdapter.Me
         holder.namaMakanan.setText(mList.get(position).getNama());
         holder.deskripsiMakanan.setText(mList.get(position).getDeskripsi());
         holder.hargaMakanan.setText(mList.get(position).getHarga());
+        holder.deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DocumentReference ref = firestore.collection("DaftarMenu").document(mList.get(position).getId());
+                ref.delete()
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if(task.isSuccessful()){
+                                    Toast.makeText(activity, "Deleted", Toast.LENGTH_LONG);
+                                    mList.remove(position);
+                                    notifyDataSetChanged();
+                                } else {
+                                    Toast.makeText(activity, "Failed to delete", Toast.LENGTH_LONG);
+                                }
+                            }
+                        });
+            }
+        });
     }
+
 
     @Override
     public int getItemCount() {
@@ -41,12 +77,15 @@ public class DaftarMenuAdapter extends RecyclerView.Adapter<DaftarMenuAdapter.Me
 
     public static class MenuViewHolder extends RecyclerView.ViewHolder {
         TextView namaMakanan, deskripsiMakanan, hargaMakanan;
+        Button deleteButton;
         public MenuViewHolder(@NonNull View itemMenu) {
             super(itemMenu);
 
             namaMakanan = itemMenu.findViewById(R.id.nama_makanan);
             deskripsiMakanan = itemMenu.findViewById(R.id.deskripsi_makanan);
             hargaMakanan = itemMenu.findViewById(R.id.harga_makanan);
+            deleteButton = itemMenu.findViewById(R.id.delete);
+
         }
 
     }
